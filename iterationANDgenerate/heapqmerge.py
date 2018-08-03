e1f9e0c3fd22d72e1a43fcfb554132b8d0fd7dcd
liuhu <max_liuhu@163.com>
2018-08-04 00:08:12 +0800

    heapqmerge

--- iterationANDgenerate/heapqmerge.py
+++ iterationANDgenerate/heapqmerge.py
@@ -22,9 +22,9 @@ for c in heapq.merge(c, d):
 
 
 # 何合并两个排序文件
-with open('sorted_file_1', 'rt') as file1, \
-    open('sorted_file_2', 'rt') as file2, \
-    open('merged_file', 'wt') as outf:
-
-    for line in heapq.merge(file1, file2):
-        outf.write(line)
+# with open('sorted_file_1', 'rt') as file1, \
+#      open('sorted_file_2', 'rt') as file2, \
+#      open('merged_file', 'wt') as outf:
+#
+#     for line in heapq.merge(file1, file2):
+#         outf.write(line)
