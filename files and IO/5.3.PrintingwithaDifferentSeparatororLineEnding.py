1b867c81ec27c20db1a90d882bb0602562a0fc01
liuhu <max_liuhu@163.com>
2018-08-07 23:37:20 +0800

    5.3. Printing with a Different Separator or Line Ending

---
+++ files and IO/5.3.PrintingwithaDifferentSeparatororLineEnding.py
@@ -0,0 +1,13 @@
+# 5.3 使用其他分隔符或行终止符打印
+
+# 以使用在 print() 函数中使用 sep 和 end 关键字参数
+print('ACME', 50, 91.5, sep=',', end='!!\n')
+
+for i in range(5):
+    print(i, end=' ')
+
+print(','.join(('ACME','50','91.5')))
+# str.join() 的问题在于它仅仅适用于字符串
+
+row = ('ACME', 50, 91.5)
+print(','.join(str(x) for x in row))
