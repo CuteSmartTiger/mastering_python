78bcd581e586e8eaed8d1d485081099f949174f0
liuhu <max_liuhu@163.com>
2018-08-07 23:37:40 +0800

    add after

--- files and IO/5.2.PrintingToAFile.py
+++ files and IO/5.2.PrintingToAFile.py
@@ -1,6 +1,9 @@
 # 将 print() 函数的输出重定向到一个文件中去
 
 # 在 print() 函数中指定 file 关键字参数:
-
 with open('test.txt', 'wt') as f:
+    print('Hello World!', file=f)
+
+# 内容追加
+with open('test.txt', 'a+') as f:
     print('Hello World!', file=f)
