555a9a54f2909e9e958c857b86f8eb27fb38dd79
liuhu <max_liuhu@163.com>
2018-08-10 23:50:49 +0800

    13.4. Prompting for a Password at Runtime

--- Utility Scripting and System Administration/13.4. Prompting for a Password at Runtime.py
+++ Utility Scripting and System Administration/13.4. Prompting for a Password at Runtime.py
@@ -1,1 +1,22 @@
 # 13.4. Prompting for a Password at Runtime
+
+import getpass
+# getpass 模块输入用户名是没有提示的
+user = getpass.getuser()
+# getpass 模块输入密码是隐式输入
+passwd = getpass.getpass()
+def svc_login(user,passwd):
+    passwd = str(passwd)
+    if 'Y' in user and passwd =="1111":
+        return True
+    else:
+        return False
+
+if svc_login('Y', 1111):
+    print('Yay!')
+else:
+    print('Boo!')
+
+
+# input有提示
+user = input('Enter your username: ')
