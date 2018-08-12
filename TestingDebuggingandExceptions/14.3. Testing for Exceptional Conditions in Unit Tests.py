c3f97874a83c6baa95561c6a73f4b794742bfc87
liuhu <max_liuhu@163.com>
2018-08-12 18:24:49 +0800

    14.3. Testing for Exceptional Conditions in Unit Test

---
+++ TestingDebuggingandExceptions/14.3. Testing for Exceptional Conditions in Unit Tests.py
@@ -0,0 +1,39 @@
+
+# 注意unittest.TestCase
+
+# 1.判断是否是某个函数导致了valueerror
+import unittest
+# A simple function to illustrate
+def parse_int(s):
+    return int(s)
+
+class TestConversion(unittest.TestCase):
+    def test_bad_int(self):
+        self.assertRaises(ValueError, parse_int, 'N/A')
+
+
+# 2.以某种方式检查异常的值
+import errno
+class TestIO(unittest.TestCase):
+    def test_file_not_found(self):
+        try:
+            f = open('/file/not/found')
+        except IOError as e:
+            self.assertEqual(e.errno, errno.ENOENT)
+        else:
+            self.fail('IOError not raised')
+
+# 3.使用try except方法如下：
+class TestConversion(unittest.TestCase):
+    def test_bad_int(self):
+        try:
+            r = parse_int('N/A')
+        except ValueError as e:
+            self.assertEqual(type(e), ValueError)
+        else:
+            self.fail('ValueError not raised')
+
+# 使用assertRaisesRegex
+class TestConversion(unittest.TestCase):
+    def test_bad_int(self):
+        self.assertRaisesRegex(ValueError, 'invalid literal .*',parse_int, 'N/A')
