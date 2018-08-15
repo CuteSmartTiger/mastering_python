0e825ec8a8c06a5937fe1002b87bba65ebd557b6
liuhu <max_liuhu@163.com>
2018-08-16 00:13:27 +0800

    12.6. Storing Thread-Specific State

---
+++ Concurrency/12.6. Storing Thread-Specific State.py
@@ -0,0 +1,47 @@
+# 12.6. Storing Thread-Specific State
+
+# 目的：保存正在运行线程的状态，这个状态对于其他的线程是不可见的
+
+# 方法：使用 thread.local() 创建一个本地线程存储对象。对这个对象
+# 的属性的保存和读取操作都只会对执行线程可见，而其他线程并不可见。
+from socket import socket, AF_INET, SOCK_STREAM
+import threading
+
+class LazyConnection:
+    def __init__(self, address, family=AF_INET, type=SOCK_STREAM):
+        self.address = address
+        self.family = AF_INET
+        self.type = SOCK_STREAM
+        self.local = threading.local()
+
+    def __enter__(self):
+        if hasattr(self.local, 'sock'):
+            raise RuntimeError('Already connected')
+        self.local.sock = socket(self.family, self.type)
+        self.local.sock.connect(self.address)
+        return self.local.sock
+
+    def __exit__(self, exc_ty, exc_val, tb):
+        self.local.sock.close()
+        del self.local.sock
+
+from functools import partial
+def test(conn):
+    with conn as s:
+        # socket下的send()方法，发送TCP数据，返回发送的字节大小
+        s.send(b'GET /index.html HTTP/1.0\r\n')
+        s.send(b'Host: www.python.org\r\n')
+
+        s.send(b'\r\n')
+        resp = b''.join(iter(partial(s.recv, 8192), b''))
+
+    print('Got {} bytes'.format(len(resp)))
+
+if __name__ == '__main__':
+    conn = LazyConnection(('www.python.org', 80))
+    t1 = threading.Thread(target=test, args=(conn,))
+    t2 = threading.Thread(target=test, args=(conn,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
