2b72ca0fb6c082727bdc4a2eb503e26dd4ae9d3b
liuhu <max_liuhu@163.com>
2018-08-16 23:35:09 +0800

    12.7. Creating a Thread Pool

---
+++ Concurrency/12.7. Creating a Thread Pool.py
@@ -0,0 +1,122 @@
+# 12.7. Creating a Thread Pool
+
+# 简单的TCP服务器，使用了一个线程池来响应客户端
+# concurrent.futures 函数库有一个 ThreadPoolExecutor 类
+from socket import AF_INET, SOCK_STREAM, socket
+from concurrent.futures import ThreadPoolExecutor
+
+def echo_client(sock, client_addr):
+    '''
+    Handle a client connection
+    '''
+    print('Got connection from', client_addr)
+    while True:
+        msg = sock.recv(65536)
+        if not msg:
+            break
+        sock.sendall(msg)
+    print('Client closed connection')
+    sock.close()
+
+def echo_server(addr):
+    pool = ThreadPoolExecutor(128)
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(addr)
+    sock.listen(5)
+    while True:
+        client_sock, client_addr = sock.accept()
+        pool.submit(echo_client, client_sock, client_addr)
+
+echo_server(('',15000))
+
+# 手动创建你自己的线程池， 通常可以使用一个Queue来轻松实现
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+from queue import Queue
+
+def echo_client(q):
+    '''
+    Handle a client connection
+    '''
+    sock, client_addr = q.get()
+    print('Got connection from', client_addr)
+    while True:
+        msg = sock.recv(65536)
+        if not msg:
+            break
+        sock.sendall(msg)
+    print('Client closed connection')
+
+    sock.close()
+
+def echo_server(addr, nworkers):
+    # Launch the client workers
+    q = Queue()
+    for n in range(nworkers):
+        t = Thread(target=echo_client, args=(q,))
+        t.daemon = True
+        t.start()
+
+    # Run the server
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(addr)
+    sock.listen(5)
+    while True:
+        client_sock, client_addr = sock.accept()
+        q.put((client_sock, client_addr))
+
+echo_server(('',15000), 128)
+
+
+# 使用 ThreadPoolExecutor 使得 任务提交者更方便的从被调用函数中获取返回值
+
+from concurrent.futures import ThreadPoolExecutor
+import urllib.request
+
+def fetch_url(url):
+    u = urllib.request.urlopen(url)
+    print(u)
+    data = u.read()
+    print(data)
+    return data
+
+
+pool = ThreadPoolExecutor(10)
+# Submit work to the pool
+a = pool.submit(fetch_url, 'http://www.python.org')
+b = pool.submit(fetch_url, 'http://www.google.com')
+
+# Get the results back
+x = a.result()
+y = b.result()
+
+
+# 防止线程无线增长
+from threading import Thread
+from socket import socket, AF_INET, SOCK_STREAM
+
+def echo_client(sock, client_addr):
+    '''
+    Handle a client connection
+    '''
+    print('Got connection from', client_addr)
+    while True:
+        msg = sock.recv(65536)
+        if not msg:
+            break
+        sock.sendall(msg)
+    print('Client closed connection')
+    sock.close()
+
+def echo_server(addr, nworkers):
+    # Run the server
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(addr)
+    sock.listen(5)
+    while True:
+        client_sock, client_addr = sock.accept()
+        t = Thread(target=echo_client, args=(client_sock, client_addr))
+        t.daemon = True
+        t.start()
+
+echo_server(('',15000))
