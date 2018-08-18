9dcfe41489ce2f222711bb19ca15cb76465482e7
liuhu <max_liuhu@163.com>
2018-08-18 14:08:31 +0800

    运行失败

---
+++ NetworkAndWebProgramming/11.10.1support.py
@@ -0,0 +1,10 @@
+# Adding SSL to Network Services
+
+from socket import socket, AF_INET, SOCK_STREAM
+import ssl
+s = socket(AF_INET, SOCK_STREAM)
+s_ssl = ssl.wrap_socket(s,cert_reqs=ssl.CERT_REQUIRED,ca_certs = 'server_cert.pem')
+s_ssl.connect(('localhost', 20000))
+s_ssl.send(b'Hello World?')
+# 12
+s_ssl.recv(8192)
