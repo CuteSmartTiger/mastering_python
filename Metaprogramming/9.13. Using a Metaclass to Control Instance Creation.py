99ac36cb3ee3cc36eb5bbdf9a3d286717bdafe83
liuhu <max_liuhu@163.com>
2018-08-23 23:37:56 +0800

    9.14. Capturing Class Attribute Definition Order

--- Metaprogramming/9.13. Using a Metaclass to Control Instance Creation.py
+++ Metaprogramming/9.13. Using a Metaclass to Control Instance Creation.py
@@ -1,1 +1,14 @@
 # 9.13. Using a Metaclass to Control Instance Creation
+
+# 1.创建一个不可以实例化的类，只能用静态方法调用属性
+# 思路：
+# 1.定义一个元类并自己实现 __call__() 方法，方法中报错
+class NoInstances(type):
+    def __call__(self, *args, **kwargs):
+        raise TypeError("Can't instantiate directly")
+
+# Examp
+class Spam(metaclass=NoInstances):
+    @staticmethod
+    def grok(x):
+        print('Spam.grok')
