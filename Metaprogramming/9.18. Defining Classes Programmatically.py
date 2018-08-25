bf17cac8c746ce5ee44b6fd0bc6b0bf297a3a316
liuhu <max_liuhu@163.com>
2018-08-25 10:32:08 +0800

    9.18. Defining Classes Programmatically

--- Metaprogramming/9.18. Defining Classes Programmatically.py
+++ Metaprogramming/9.18. Defining Classes Programmatically.py
@@ -1,53 +1,45 @@
 # 9.18. Defining Classes Programmatically
 
+# 目的：将类的定义源代码以字符串的形式发布出去。 并且使用函数比如 exec() 来执行它
 
-# 2.作为更高级和实用的例子，下面有一个元类，它用来检测
-# 重载方法，确保它的调用参数跟父类中原始方法有着相同
-# 的参数签名
+#
+# 可以使用函数 types.new_class() 来初始化新的类对象。
+# 你需要做的只是提供类的名字、父类元组、关键字参数，
+# 以及一个用成员变量填充类字典的回调函数
 
-from inspect import signature
-import logging
+# stock.py
+# Example of making a class manually from parts
 
-class MatchSignaturesMeta(type):
+# Methods
+def __init__(self, name, shares, price):
+    self.name = name
+    self.shares = shares
+    self.price = price
+def cost(self):
+    return self.shares * self.price
 
-    def __init__(self, clsname, bases, clsdict):
-        super().__init__(clsname, bases, clsdict)
-        sup = super(self, self)
-        for name, value in clsdict.items():
-            if name.startswith('_') or not callable(value):
-                continue
-            # Get the previous definition (if any) and compare the signatures
-            prev_dfn = getattr(sup,name,None)
-            if prev_dfn:
-                prev_sig = signature(prev_dfn)
-                val_sig = signature(value)
-                if prev_sig != val_sig:
-                    logging.warning('Signature mismatch in %s. %s != %s',
-                                    value.__qualname__, prev_sig, val_sig)
+cls_dict = {
+    '__init__' : __init__,
+    'cost' : cost,
+}
 
-# Example
-class Root(metaclass=MatchSignaturesMeta):
+# Make a class
+import types
+
+Stock = types.new_class('Stock', (), {}, lambda ns: ns.update(cls_dict))
+Stock.__module__ = __name__
+# 对 Stock.__module__ 的赋值。， 每次当一个类被定义后，它的
+# __module__ 属性包含定义它的模块名。 这个名字用于生成 __repr__() 方法的输出
+
+# 2.想创建的类需要一个不同的元类，可以通过 types.new_class() 第三个参数传递
+import abc
+Stock = types.new_class('Stock', (), {'metaclass': abc.ABCMeta},lambda ns: ns.update(cls_dict))
+
+# 第三个参数还可以包含其他的关键字参数
+class Spam(Base, debug=True, typecheck=False):
     pass
+# 那么可以将其翻译成如下的 new_class() 调用形式：
 
-class A(Root):
-    def foo(self, x, y):
-        pass
-
-    def spam(self, x, *, z):
-        pass
-
-# Class with redefined methods, but slightly different signatures
-class B(A):
-    def foo(self, a, b):
-        pass
-
-    def spam(self,x,z):
-        pass
-
-# WARNING:root:Signature mismatch in B.spam. (self, x, *, z) != (self, x, z)
-# WARNING:root:Signature mismatch in B.foo. (self, x, y) != (self, a, b)
-
-# 代码中有一行使用了 super(self, self) 并不是排版错误。 当使用元
-# 类的时候，我们要时刻记住一点就是 self 实际上是一个类对象。 因此
-
-# ，这条语句其实就是用来寻找位于继承体系中构建 self 父类的定义。
+Spam = types.new_class('Spam', (Base,),
+                        {'debug': True, 'typecheck': False},
+                        lambda ns: ns.update(cls_dict))
