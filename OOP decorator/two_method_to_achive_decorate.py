b3aebb213203552a7a9ff128e640000be91ad3e0
liuhu <max_liuhu@163.com>
2018-09-13 23:04:33 +0800

    装饰器的用法及另外两种同样实现装饰器效果的方法

---
+++ OOP decorator/two_method_to_achive_decorate.py
@@ -0,0 +1,47 @@
+# 方法一：
+def check(func):
+    def inner():
+        print('check password')
+        func()
+    return inner
+
+@check
+def log():
+    print('loging')
+
+log()
+
+
+print('-------------------方法2-----------')
+
+def check(func):
+    def inner():
+        print('check password')
+        func()
+    return inner
+
+
+def log():
+    print('loging')
+
+log = check(log)
+log()
+
+print('---------------使用类实现装饰器-----------')
+class check:
+    def __init__(self,func):
+        self.fan = func
+
+    # 定义call方法方可调用实例
+    def __call__(self, *args, **kwargs):
+        print('check password')
+        # 调用时执行函数
+        self.fan()
+
+def log():
+    print('loging')
+
+# 通过check类创建实例对象
+log = check(log)
+# 加上()，调用call方法
+log()
