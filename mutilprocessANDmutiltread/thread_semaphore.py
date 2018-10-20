1f0391d3f15f3808e4020f19de72488aa50e2916
max_liuhu <ecustbaihu@163.com>
2018-10-20 23:15:08 +0800

    thread_semaphore

---
+++ mutilprocessANDmutiltread/thread_semaphore.py
@@ -0,0 +1,38 @@
+# semaphore 控制每次允许的最大并发线程
+
+import threading
+import time
+
+
+class SpiderConsumer(threading.Thread):
+    def __init__(self, url, name, sema):
+        super().__init__(name=name)
+        self.url = url
+        self.sema = sema
+
+    def run(self):
+        time.sleep(1)
+        print('#{0}'.format(self.name), '当前页面爬取成功')
+        self.sema.release()
+
+
+class UrlProducer(threading.Thread):
+    def __init__(self, sema):
+        self.sema = sema
+        super().__init__(name='url produce')
+
+    def run(self):
+        for i in range(15):
+            url = 'www.baidu.com/{0}/'.format(i)
+            # 获取锁，锁住五个线程，当value值为0时，释放锁，然后重新开辟五个线程
+            self.sema.acquire()
+            spider = SpiderConsumer(url, i, self.sema)
+            # 每执行一次start，sema中的value值减小1,
+            spider.start()
+
+
+if __name__ == '__main__':
+    # 单次允许的最大开辟的线程
+    sema = threading.Semaphore(value=4)
+    url_producer = UrlProducer(sema)
+    url_producer.start()
