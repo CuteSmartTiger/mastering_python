5deaebc71abf1f1c1538128ba792b16fe0c36bc4
max_liuhu <ecustbaihu@163.com>
2018-10-20 11:31:16 +0800

    threads_share_varables

--- mutilprocessANDmutiltread/thread_queue.py
+++ mutilprocessANDmutiltread/thread_queue.py
@@ -1,52 +1,55 @@
+# 本模块需要了解的知识点：
+# queue中使用的deque，get,put，join，empty，qsize等方法
+# 使用queue的原因，为什么会安全
+# 用queue来实现线程之间的安全通信，主要指再字节码或时间片内时安全的
 import threading
 import time
 from random import randint
+from queue import Queue
 
-url_list = []
 
-
-def get_url_list():
-    global url_list
+def get_url_list(url_queue):
     print('get_url_list:start')
     while True:
         for i in range(10):
-            url_list.append('www.studyai.com/' + str(randint(1, 2000)) + '/')
-        time.sleep(2.5)
+            url_queue.put('www.studyai.com/' + str(randint(1, 2000)) + '/')
+        time.sleep(2)
         print('生产者有生产了10个URL')
 
 
-def get_html_detail(id):
-    global url_list
-    print('get_html_detail:start')
+def get_html_detail(id, url_queue):
+    print(str(id), 'get_html_detail:start')
     # 模拟爬取信息，每次爬取一个网页需要时间为1秒
     while True:
-        print('剩余URL个数：', len(url_list))
-        # 此处判断为0时，很有可能获取urllist线程会向其中添加url，所以是不安全的
-        # 所以后续用使用queue来实现线程之间的安全通信，主要指再字节码或时间片内时安全的
-        if len(url_list) > 0:
-            url = url_list.pop()
+        print('剩余URL个数：', url_queue.qsize())
+        if not url_queue.empty():
+            url = url_queue.get()
             print(str(id), '正在获取详情:', url)
             time.sleep(1)
         else:
             # print('get_html_detail:end')
-            print('URL个数为空')
+            # print('URL个数为空')
+            print('URL队列为空')
             # break
 
 
 if __name__ == "__main__":
     time_start = time.time()
+    url_queue = Queue(maxsize=50)
     # 如果此处daemon=True，则生产者与消费者不会循环,
     # 此时主线程结束后会关闭子线程
-    list_thread = threading.Thread(target=get_url_list, daemon=False)
+    list_thread = threading.Thread(target=get_url_list, args=(url_queue,), daemon=False)
     list_thread.start()
     # 此处不可以使用join，否则会陷入while true的不断执行中
     # list_thread.join()
     detail_threads = []
     for i in range(5):
-        detail_thread = threading.Thread(target=get_html_detail, args=(i,),
+        detail_thread = threading.Thread(target=get_html_detail, args=(i, url_queue),
                                          daemon=False)
         detail_threads.append(detail_thread)
         detail_thread.start()
         # detail_thread.join()
+    # 主线程会等待子线程结束后再继续执行
+    url_queue.join()
     consumed_time = time.time() - time_start
     print(consumed_time)
