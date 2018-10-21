d751eb3505cb54990441fde4dfe4c59c3e10d47d
max_liuhu <ecustbaihu@163.com>
2018-10-21 18:21:02 +0800

    mutilprocess_fork

---
+++ mutilprocessANDmutiltread/mutilprocess_fork.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+# fork 只用于Linux或Unix系统中，会返回两次
+# 子进程或复制父进程
+print('here')
+pid = os.fork()
+# print('here')
+print(pid)
+if pid ==0:
+    print('子进程id是{0}，父进程id是{1}'.format(os.getpid(),os.getpid()))
+else:
+    print('我是父进程id：{0}'.format(pid))
