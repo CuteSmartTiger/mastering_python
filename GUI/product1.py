07527b1c7c77407b035e6e023381201f9c0a3060
max_liuhu <ecustbaihu@163.com>
2018-11-05 17:24:50 +0800

    Update GUI/product1.py

---
+++ GUI/product1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# 创建窗口对象的背景色
+root = Tk()
+
+Label(root, text='VM INFO', bg='white').pack(side=LEFT, anchor=W, fill=X, expand=YES)
+
+root.title('SHOW DATA')
+root.geometry("400x100")
+root.mainloop()
