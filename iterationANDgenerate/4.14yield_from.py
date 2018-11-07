0126ac02484e7446d45f036420869e9e60c57da8
max_liuhu <ecustbaihu@163.com>
2018-11-07 14:21:19 +0800

    Update iterationANDgenerate/4.14yield_from.py

--- iterationANDgenerate/4.14yield_from.py
+++ iterationANDgenerate/4.14yield_from.py
@@ -4,6 +4,8 @@
 # 2.运行的机理
 
 from collections import Iterable
+
+
 def flatten(items, ignore_types=(str, bytes)):
     for x in items:
         if isinstance(x, Iterable) and not isinstance(x, ignore_types):
@@ -11,9 +13,12 @@ def flatten(items, ignore_types=(str, bytes)):
         else:
             yield x
 
+
 items = [1, 2, [3, 4, [5, 6], 7], 8]
 for x in flatten(items):
-    print(x,end=',')
+    print(x, end=',')
+
+
 # 1,2,3,4,5,6,7,8,
 
 # 若不用yield from 则
