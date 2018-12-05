e144f925a2314f42a9ca88e7efe80a8988cb4ddb
max_liuhu <ecustbaihu@163.com>
2018-12-05 23:36:54 +0800

    Create __init__.py

---
+++ python_datastructures/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/5 22:39
+# @Author  : liuhu
+# @Site    : 
+# @File    : __init__.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
