67b328b336d8584d912826e56693b69b0182e3cc
max_liuhu <ecustbaihu@163.com>
2018-12-18 09:10:48 +0800

    Update mutilprocess_pool.py

--- mutilprocessANDmutiltread/mutilprocess_pool.py
+++ mutilprocessANDmutiltread/mutilprocess_pool.py
@@ -35,14 +35,14 @@ if __name__ == '__main__':
     print("result进程结果:{0}".format(result.get()))
     print("result1进程结果:{0}".format(result1.get()))
 
-    print('============= 进程次中使用imap ========================')
+    print('============= 进程池中使用imap ========================')
     # imap执行多个子进程,阻塞式依次完成，与imap_unordered源码对比
     # 可知其内部实现机制为需要返回进程结果才可以返回值
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
     for result in pool.imap(spider,[1,4,2,3]):
         print('{0} sleep ended'.format(result))
 
-    print('========= 进程次中使用imap_unordered ===========')
+    print('========= 进程池中使用imap_unordered ===========')
     # imap_unordered，非阻塞式
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
     for result in pool.imap_unordered(spider, [1, 4, 2, 3]):
