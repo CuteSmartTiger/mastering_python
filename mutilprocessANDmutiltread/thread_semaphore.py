3891495723b1da74fed716d2003a92f351f47903
max_liuhu <ecustbaihu@163.com>
2018-12-18 09:11:04 +0800

    Update thread_semaphore.py

--- mutilprocessANDmutiltread/thread_semaphore.py
+++ mutilprocessANDmutiltread/thread_semaphore.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# semaphore 控制每次允许的最大并发线程
+# semaphore 控制每次允许的最大并发线程数
+# 线程池同样可以实现这个效果
+
+# 使用场景：
+# 1.比如文件的写入，单次并发最大线程数为1
+
+# 本节需要掌握的知识点
+# queue中的Queue
 
 import threading
 import time
@@ -12,9 +19,13 @@ class SpiderConsumer(threading.Thread):
         self.url = url
         self.sema = sema
 
+    # 这里重写了父类中的run方法
     def run(self):
         time.sleep(1)
         print('#{0}'.format(self.name), '当前页面爬取成功')
+        # 爬取成功一次释放一次锁，通过看源码
+        # 可知，释放一次，则value加1，锁全部
+        # 释放，Semaphore中使用了Condition实现同步
         self.sema.release()
 
 
@@ -23,13 +34,16 @@ class UrlProducer(threading.Thread):
         self.sema = sema
         super().__init__(name='url produce')
 
+    # 这里重写了父类中的run方法
     def run(self):
         for i in range(15):
             url = 'www.baidu.com/{0}/'.format(i)
-            # 获取锁，锁住五个线程，当value值为0时，释放锁，然后重新开辟五个线程
+            # 获取锁，锁住五个线程，当value值为0时，
+            # 释放锁，然后重新开辟五个线程
+            # 每执行一次acquire，sema中的value值减小1,
+            # value等于0零时调用wait方法，然后阻塞
             self.sema.acquire()
             spider = SpiderConsumer(url, i, self.sema)
-            # 每执行一次start，sema中的value值减小1,
             spider.start()
 
 
