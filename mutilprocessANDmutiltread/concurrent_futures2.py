7b207c88ced8bbb2cfd2cf716cc0b99d2a393ba6
max_liuhu <ecustbaihu@163.com>
2018-12-18 09:10:36 +0800

    Update concurrent_futures2.py

--- mutilprocessANDmutiltread/concurrent_futures2.py
+++ mutilprocessANDmutiltread/concurrent_futures2.py
@@ -4,6 +4,9 @@ import time
 from concurrent import futures
 
 
+# 穷举法比较低级，当需要已经成功完成的
+# 任务的返回值时，应该使用as_completed()
+
 def spider(id_num, times):
     time.sleep(times)
     print('#{0}--->当前页面爬取成功！！'.format(id_num))
@@ -21,26 +24,32 @@ def spiders(params):
 # 线程池初实例化对象时传递一个线程数量上限的参数
 executor = futures.ThreadPoolExecutor(max_workers=2)
 
-# as_completed本模块主要实现功为：获取已经执行完毕的子线程，还可以获取子线程的返回结果
+# as_completed本模块主要实现功为：获取已经
+# 执行完毕的子线程，还可以获取子线程的返回结果
 urls = [(1, 2), (2, 1), (3, 1.5), (4, 2.5)]
 all_tasks = [executor.submit(spider, id_num, times) for id_num, times in urls]
-# 可以详细了解as_completed的运行机理，可能迭代时列表生成器中有部分已经执行完毕
+# 可以详细了解as_completed的运行机理，可能迭代
+# 时列表生成器中有部分已经执行完毕
 for future in futures.as_completed(all_tasks):
+    # result获取的是被调用函数return返回的结果，可以定制
+    # 函数，获取多线程运行后的结果
     result = future.result()
     # 输出顺序由执行时间与列表生产式有关
-    print(result)
+    print('线程池中线程的执行结果：{0}'.format(result))
 
 print('------------------')
 urls = [(1, 2), (2, 1), (3, 1.5), (4, 2.5)]
 all_tasks = [executor.submit(spider, id_num, times) for id_num, times in urls]
+# 当有一个函数执行后主线程执行print方法，线程池里继续执行其他线程相关的任务
 futures.wait(all_tasks, return_when=futures.FIRST_COMPLETED)
-print('已经有一个线程执行完毕')
+print('已经有一个线程执行完毕')8
 
 print('------------------')
 urls = [(1, 2), (2, 1), (3, 1.5), (4, 2.5)]
 all_tasks = [executor.submit(spider, id_num, times) for id_num, times in urls]
+# wait方法，等待所有线程完毕后才会继续执行print函数
 futures.wait(all_tasks)
-print('已经有一个线程执行完毕')
+print('wait测试，已经有一个线程执行完毕')
 
 print('------------------')
 # 针对的executor通过map方法获取子线程的返回值
