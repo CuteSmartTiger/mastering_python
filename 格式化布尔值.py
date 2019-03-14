d937fd349555811e2f5b446dcf64afeb9882d7de
MAX-liuhu <max_liuhu@163.com>
2019-03-14 10:40:42 +0800

    Create 格式化布尔值.py

---
+++ 格式化布尔值.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/14 10:25
+# @Author  : liuhu
+# @File    : 格式化布尔值.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+def test(num):
+    if num > 0:
+        return True
+    else:
+        return False
+res = test(2)
+print 'flasdjfi %s'%(res)
