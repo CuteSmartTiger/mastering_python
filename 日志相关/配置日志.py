29fe38e7b54a47b68833b3bc0ac247e06091fa7e
MAX-liuhu <max_liuhu@163.com>
2019-03-18 20:50:01 +0800

    Create 配置日志.py

---
+++ 日志相关/配置日志.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+# @Time    : 2019/3/18 16:56
+# @Author  : liuhu
+# @File    : 配置日志.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+import logging
+
+
+LOG_FORMAT = "%(asctime)s - %(levelname)s-%(name)s:%(message)s"
+logging.basicConfig(filename='vdimonitor.log', level=logging.DEBUG, format=LOG_FORMAT)
+def test_log():
+    try:
+        1/0
+    except ZeroDivisionError as e:
+        logging.debug('test %s',exc_info = True)
+    else:
+        print '测试日志'
+test_log()
+
