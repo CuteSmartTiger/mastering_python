030d32daedd8cf9b148a87838e29a8b944bb3779
MAX-liuhu <max_liuhu@163.com>
2019-03-27 20:27:35 +0800

    Create 使用装饰器对函数进行性能测试.py

---
+++ 性能测试/使用装饰器对函数进行性能测试.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/21 11:57
+# @Author  : liuhu
+# @File    : 使用装饰器对函数进行性能测试.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+
+import time
+from functools import wraps
+
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        r = func(*args, **kwargs)
+        end = time.time()
+        print('{}.{} : {}'.format(func.__module__, func.__name__, end - start))
+        return r
+    return wrapper
+
+
+@timethis
+def countdown(n):
+    while n > 0:
+        n -= 1
+
+
+countdown(10000)
