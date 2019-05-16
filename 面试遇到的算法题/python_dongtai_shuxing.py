6a870fa1e4466988424d94a4cb8c4ad18993f578
MAX-liuhu <max_liuhu@163.com>
2019-05-16 09:33:44 +0800

    Create python_dongtai_shuxing.py

---
+++ 面试遇到的算法题/python_dongtai_shuxing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/15 15:20
+# @Author  : liuhu
+# @File    : python_dongtai_shuxing.py
+# @Software: PyCharm
+# @github  :https://github.com/Max-Liuhu
+
+
+class Test(object):
+    name = 'liuhu'
+    pass
+
+
+p = Test()
+p.name = 'liuhu'
+print p.name
+
+
+print dir(Test)
